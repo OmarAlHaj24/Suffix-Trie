285722939b743a04bb7309c1e9b190d38eaee4f2
Omar Al Haj <omaralhaj24@gmail.com>
2021-12-17 17:23:52 +0200

    ..

---
+++ cmake-build-debug/CMakeFiles/Suffix_Trie.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Suffix_Trie.dir/main.cpp.obj"
+  "Suffix_Trie.exe"
+  "Suffix_Trie.exe.manifest"
+  "Suffix_Trie.pdb"
+  "libSuffix_Trie.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Suffix_Trie.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
